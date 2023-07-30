8f4a95d6e4920134044e37b3595a7fe9a8a89075
jobmania <xzxz7003@naver.com>
2023-07-30 14:46:30 +0900

    < JPA 활용-1 > :  엔티티 설계 시작

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id @Ge
+}
