4fcaa55560d1a867b99637f42fe1f333979b4087
jobmania <xzxz7003@naver.com>
2023-08-08 17:52:22 +0900

    <JPA 활용 - 1편 > : 영속성 컨텍스트 기능
    - 1차캐시는 트랜잭션단위로 구성되어있다. 
      그래서 다른 트랜잭션끼리는 캐시를 제공하지 않음.
    
    -  동일성을 보장:  1차 캐시로 반복 가능한 읽기(REPEATABLE READ) 등급의 트랜잭
    션 격리 수준을 데이터베이스가 아닌 애플리케이션 차원에서 제공
    
    - 쓰기지연 : 커밋하는 순간 SQL을 실행 ( 버퍼 WRITE)
     batch size만 큼 모았다가 보낸다. 
    
    - 변경감지 :   변경을 감지해서 update 쿼리를 생성! 
     ++ 값만 바꾸고 persist를 호출하지 않는 것이 정답.

--- jpabasic/src/main/java/hellojpa/JpaMain.java
+++ jpabasic/src/main/java/hellojpa/JpaMain.java
@@ -18,28 +18,50 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 1. 데이터 저장
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("엄준식");
-//            em.persist(member);
+//            // 1. 데이터 저장
+//            Member member2 = new Member();
+//            member2.setId(12L);
+//            member2.setName("영숙아니고 영속");
+//            em.persist(member2);
+//
+//            // 2. 데이터 조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember = " + findMember.getName());
+//            //  트랜잭션이 커밋하는 시점에서 체크를 해서 update 쿼리가 날라간다.
+//            findMember.setName("수정이다!");
+//
+//            // 3. 특정 조건에 대해 검색시 JPQL 사용
+//            List<Member> result = em.createQuery("select m from Member m", Member.class)
+//                    .setFirstResult(1) // 페이지 네이션 1번째부터 10개들고와!  limit
+//                    .setMaxResults(10) // offset
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println(member.getName());
+//            }
 
-            // 2. 데이터 조회
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember = " + findMember.getName());
-            //  트랜잭션이 커밋하는 시점에서 체크를 해서 update 쿼리가 날라간다.
-            findMember.setName("수정이다!");
 
-            // 3. 특정 조건에 대해 검색시 JPQL 사용
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .setFirstResult(1) // 페이지 네이션 1번째부터 10개들고와!  limit
-                    .setMaxResults(10) // offset
-                    .getResultList();
+            // 쿼리생성시점
 
-            for (Member member : result) {
-                System.out.println(member.getName());
-            }
+//            System.out.println("=====BEFORE======");
+//            Member member3 = new Member();
+//            member3.setId(101L);
+//            member3.setName("검사용");
+//            em.persist(member3);
+//            System.out.println("=====AFTER======");
+
+
+            // 영속, commit 이 된순간 query가 날라간다
+            // 모아쓰기! 
+            Member member1 = new Member(150L, "1");
+
+            Member member = em.find(Member.class, 150L);
+
+            // 값만 바꿧지만, 변경을 감지해서 update 쿼리를 생성!
+            member.setName("바꾸기!!");
             
+            System.out.println("===========");
+
             tx.commit();
 
         }catch (Exception e){
