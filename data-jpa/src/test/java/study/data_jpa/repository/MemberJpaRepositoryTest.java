5f0db383ad712042bc5b63191cb82f51509fbc25
jobmania <xzxz7003@naver.com>
2024-06-12 00:51:02 +0900

    jpa 인터페이스 ..

--- data-jpa/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
+++ data-jpa/src/test/java/study/data_jpa/repository/MemberJpaRepositoryTest.java
@@ -3,14 +3,17 @@ package study.data_jpa.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
-//@Rollback(value = false)
+@Rollback(value = false)
 class MemberJpaRepositoryTest {
 
     @Autowired
@@ -27,5 +30,36 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
         assertThat(findMember).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member = new Member("member1", 12);
+        Member member2 = new Member("member2", 32);
+
+        memberJpaRepository.save(member);
+        memberJpaRepository.save(member2);
+        Member findMember1 = memberJpaRepository.findById(member.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회
+        List<Member> all = memberJpaRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제
+        memberJpaRepository.delete(member);
+        memberJpaRepository.delete(member2);
+
+        // 삭제후 검증
+
+        long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
     }
 }
--- data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +21,7 @@ class MemberRepositoryTest {
     private MemberRepository memberRepository;
     @Test
     void testMethod() {
+        System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
         Member member = new Member("memberA", 2);
         Member savedMember = memberRepository.save(member);
 
@@ -28,6 +30,37 @@ class MemberRepositoryTest {
         assertThat(findMember.get().getId()).isEqualTo(member.getId());
         assertThat(findMember.get().getUserName()).isEqualTo(member.getUserName());
         assertThat(findMember.get()).isEqualTo(member);
+    }
+
+    @Test
+    public void basicCRUD(){
+        Member member = new Member("member1", 12);
+        Member member2 = new Member("member2", 32);
+
+        memberRepository.save(member);
+        memberRepository.save(member2);
+        Member findMember1 = memberRepository.findById(member.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        // 카운트
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제
+        memberRepository.delete(member);
+        memberRepository.delete(member2);
+
+        // 삭제후 검증
+
+        long deleteCount = memberRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
     }
 
 }
